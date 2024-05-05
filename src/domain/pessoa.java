98817b4a04cd5bec1c242ff29f108554ae02f46f
user <gabrieleleuterio990@gmail.com>
2024-05-05 20:27:22 -0300

    Adição do método apresentarse() à classe Pessoa e respectivo teste

--- src/domain/pessoa.java
+++ src/domain/pessoa.java
@@ -9,4 +9,11 @@ public class pessoa {
     String endereco;
     LocalDate nascimento;
     BigDecimal salario;
+
+    public void apresentarse() {
+        System.out.println("Nome......: " + this.nome);
+        System.out.println("Endereço..: " + this.endereco);
+        System.out.println("Nascimento: " + this.nascimento);
+        System.out.println("Salário...: " + this.salario);
+    }
 }
